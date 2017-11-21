ecfe3eed6372ccf707214b6d1921fa1a2ccb8c3f
Ryan Lee <ryantlee9@gmail.com>
2017-11-20 16:47:32 -0800

    Update animation fn: accept figsize as arg

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,8 @@ import numpy as np
 plt.rc('animation', html='html5')
 
 
-def animate_scan(image):
-    fig = plt.figure(figsize=(16, 16))
+def animate_scan(image, fig_size=(16, 16)):
+    fig = plt.figure(figsize=fig_size)
     ax = fig.add_subplot(111)
 
     def animate(i):
@@ -17,6 +17,6 @@ def animate_scan(image):
     return animation.FuncAnimation(fig, animate, frames=range(0, image.shape[2]), interval=200, blit=True)
 
 
-def load_animate_scan(path):
+def load_animate_scan(path, *args, **kwargs):
     image = tsa.read_data(path)
-    return animate_scan(image)
+    return animate_scan(image, *args, **kwargs)
