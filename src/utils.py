f7b15ff5fd0bea0ee2d043927a41b9e009b05039
Ryan Lee <ryantlee9@gmail.com>
2017-11-20 16:39:18 -0800

    Refactor scan animation - support image and path as args

--- src/utils.py
+++ src/utils.py
@@ -6,13 +6,17 @@ import numpy as np
 plt.rc('animation', html='html5')
 
 
-def animate_scan(path):
-    data = tsa.read_data(path)
+def animate_scan(image):
     fig = plt.figure(figsize=(16, 16))
     ax = fig.add_subplot(111)
 
     def animate(i):
-        im = ax.imshow(np.flipud(data[:, :, i].transpose()), cmap='viridis')
+        im = ax.imshow(np.flipud(image[:, :, i].transpose()), cmap='viridis')
         return [im]
 
-    return animation.FuncAnimation(fig, animate, frames=range(0, data.shape[2]), interval=200, blit=True)
+    return animation.FuncAnimation(fig, animate, frames=range(0, image.shape[2]), interval=200, blit=True)
+
+
+def load_animate_scan(path):
+    image = tsa.read_data(path)
+    return animate_scan(image)
