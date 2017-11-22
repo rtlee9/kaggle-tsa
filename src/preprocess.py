18cea41db584c6382b1b9a24e7c5ea438884c914
Ryan Lee <ryantlee9@gmail.com>
2017-11-21 17:48:50 -0800

    Preprocess 3D image composite

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,184 +1,86 @@
+"""Preprocess raw a3d scan images for use in model training."""
 import numpy as np
 import pandas as pd
+from skimage.transform import resize
 from os import path
-
-from timeit import default_timer as timer
+import json
 import tsahelper.tsahelper as tsa
+from tqdm import tqdm
+
+from .config import path_labels, path_a3d, path_cache, verbose, path_plots
+from .constants import IMAGE_DIM
+from .utils import save_image
 
 
-from .constants import STAGE1_LABELS, INPUT_FOLDER, PREPROCESSED_DATA_FOLDER, BATCH_SIZE, EXAMPLES_PER_SUBJECT, verbose, BINARY_IMAGE_THRESHOLD
-
-def preprocess_image(img, threat_zone, crop_dims):
-    base_img = np.flipud(img)
-    rescaled_img = tsa.convert_to_grayscale(base_img)
-    high_contrast_img = tsa.spread_spectrum(rescaled_img)
-    masked_img = tsa.roi(high_contrast_img, threat_zone)
-    cropped_img = tsa.crop(masked_img, crop_dims)
-    normalized_img = tsa.normalize(cropped_img)
+def preprocess_image(img):
+    """Scale pixel intensity, and remove mean and variance from image."""
+    rescaled_img = tsa.convert_to_grayscale(img)
+    normalized_img = tsa.normalize(rescaled_img)
     zero_centered_img = tsa.zero_center(normalized_img)
     return zero_centered_img
 
 
-def preprocess_tsa_data():
+def get_upper_bound(s, sensitivity):
+    """Get the upper bound given pixel sums along a given dimension."""
+    d0 = s - np.roll(s, 10)  # calculate the first derivative
+    d1 = d0 - np.roll(d0, 1)  # calculate the second derivative
+    d1_bin = d1 < sensitivity  # binarize the second derivative
+    return np.argmax(np.arange(s.shape[0]) * (d1_bin ^ np.roll(d1_bin, 1)))  # calculate the cutoff
 
-    # OPTION 1: get a list of all subjects for which there are labels
-    df = pd.read_csv(STAGE1_LABELS)
-    df['Subject'], df['Zone'] = df['Id'].str.split('_', 1).str
-    SUBJECT_LIST = df['Subject'].unique()
 
-    # OPTION 2: get a list of all subjects for whom there is data
-    #SUBJECT_LIST = [os.path.splitext(subject)[0] for subject in os.listdir(INPUT_FOLDER)]
-
-    # OPTION 3: get a list of subjects for small bore test purposes
-    # SUBJECT_LIST = ['00360f79fd6e02781457eda48f85da90','0043db5e8c819bffc15261b1f1ac5e42',
-    #                 '0050492f92e22eed3474ae3a6fc907fa','006ec59fa59dd80a64c85347eef810c7',
-    #                 '0097503ee9fa0606559c56458b281a08','011516ab0eca7cad7f5257672ddde70e']
-
-    # intialize tracking and saving items
-    batch_num = 1
-    threat_zone_examples = []
-    start_time = timer()
-
-    for subject in SUBJECT_LIST:
-
-        # read in the images
-        if verbose > 0:
-            print('--------------------------------------------------------------')
-            print('t+> {:5.3f} |Reading images for subject #: {}'.format(timer() - start_time, subject))
-            print('--------------------------------------------------------------')
-
-        images = tsa.read_data(INPUT_FOLDER + '/' + subject + '.aps')
-
-        # transpose so that the slice is the first dimension shape(16, 620, 512)
-        images = images.transpose()
-
-        # for each threat zone, loop through each image, mask off the zone and then crop it
-        for tz_num, (threat_zone, crop_dims) in enumerate(zip(tsa.zone_slice_list, tsa.zone_crop_list)):
-
-            # get label
-            label = np.array(tsa.get_subject_zone_label(tz_num,
-                             tsa.get_subject_labels(STAGE1_LABELS, subject)))
-
-            for img_num, img in enumerate(images):
-
-                if verbose > 1:
-                    print('Threat Zone:Image -> {}:{}'.format(tz_num, img_num))
-                    print('Threat Zone Label -> {}'.format(label))
-
-                if threat_zone[img_num] is not None:
-                    preprocessed_img = preprocess_image(img, threat_zone[img_num], crop_dims[img_num])
-                    threat_zone_examples.append([[tz_num], preprocessed_img, label])
-                else:
-                    if verbose > 1:
-                        print('-> No view of tz:{} in img:{}. Skipping to next...'.format(tz_num, img_num))
-                if verbose > 1:
-                    print('------------------------------------------------')
-
-        # each subject gets EXAMPLES_PER_SUBJECT number of examples (182 to be exact,
-        # so this section just writes out the the data once there is a full minibatch
-        # complete.
-        if ((len(threat_zone_examples) % (BATCH_SIZE * EXAMPLES_PER_SUBJECT)) == 0):
-            for tz_num, tz in enumerate(tsa.zone_slice_list):
-
-                tz_examples_to_save = []
-
-                # write out the batch and reset
-                print(' -> writing: ' + path.join(
-                    PREPROCESSED_DATA_FOLDER,
-                    'preprocessed_TSA_scans-tz{}-{}-{}-b{}.npy'.format(
-                        tz_num + 1,
-                        len(threat_zone_examples[0][1][0]),
-                        len(threat_zone_examples[0][1][1]),
-                        batch_num)))
-
-                # get this tz's examples
-                tz_examples = [example for example in threat_zone_examples if example[0] ==
-                               [tz_num]]
-
-                # drop unused columns
-                tz_examples_to_save.append([[features_label[1], features_label[2]]
-                                            for features_label in tz_examples])
-
-                # save batch.  Note that the trainer looks for tz{} where {} is a
-                # tz_num 1 based in the minibatch file to select which batches to
-                # use for training a given threat zone
-                np.save(path.join(PREPROCESSED_DATA_FOLDER,
-                        'preprocessed_TSA_scans-tz{}-{}-{}-b{}.npy'.format(
-                            tz_num + 1,
-                            len(threat_zone_examples[0][1][0]),
-                            len(threat_zone_examples[0][1][1]),
-                            batch_num)),
-                        tz_examples_to_save)
-                del tz_examples_to_save
-
-            # reset for next batch
-            del threat_zone_examples
-            threat_zone_examples = []
-            batch_num += 1
-
-    # we may run out of subjects before we finish a batch, so we write out
-    # the last batch stub
-    if (len(threat_zone_examples) > 0):
-        for tz_num, tz in enumerate(tsa.zone_slice_list):
-
-            tz_examples_to_save = []
-
-            # write out the batch and reset
-            print(' -> writing: ' + path.join(PREPROCESSED_DATA_FOLDER,
-                  'preprocessed_TSA_scans-tz{}-{}-{}-b{}.npy'.format(
-                      tz_num + 1,
-                      len(threat_zone_examples[0][1][0]),
-                      len(threat_zone_examples[0][1][1]),
-                      batch_num)))
-
-            # get this tz's examples
-            tz_examples = [example for example in threat_zone_examples if example[0] ==
-                           [tz_num]]
-
-            # drop unused columns
-            tz_examples_to_save.append([[features_label[1], features_label[2]]
-                                        for features_label in tz_examples])
-
-            # save batch
-            np.save(path.join(PREPROCESSED_DATA_FOLDER,
-                    'preprocessed_TSA_scans-tz{}-{}-{}-b{}.npy'.format(
-                        tz_num + 1,
-                        len(threat_zone_examples[0][1][0]),
-                        len(threat_zone_examples[0][1][1]),
-                        batch_num)),
-                    tz_examples_to_save)
+def get_lower_bound(s, sensitivity):
+    """Get the lower bound given pixel sums along a given dimension."""
+    d0 = s - np.roll(s, -10)  # calculate the first derivative
+    d1 = d0 - np.roll(d0, -1)  # calculate the second derivative
+    d1_bin = d1 < sensitivity  # binarize the second derivative
+    return np.argmax(np.arange(s.shape[0], 0, -1) * (d1_bin ^ np.roll(d1_bin, -1)))  # calculate the cutoff
 
 
 def crop_image(image):
     """Find the edges of a TSA scan along each dimension and return the cropped image."""
-    image_binary = (image > BINARY_IMAGE_THRESHOLD) * 1
-    image_binary.sum()
+    s0 = image.mean(axis=1).mean(axis=1)
+    s1 = image.mean(axis=0).mean(axis=1)
+    s2 = image.mean(axis=0).mean(axis=0)
 
-    s0 = image_binary.mean(axis=1).mean(axis=1)
-    s1 = image_binary.mean(axis=0).mean(axis=1)
-    s2 = image_binary.mean(axis=0).mean(axis=0)
+    top_border = get_upper_bound(s0, 2.5e-7)
+    right_border = get_upper_bound(s1, .9e-7)
+    left_border = get_lower_bound(s1, .9e-7)
+    avg_lr_border = np.floor(left_border + (s1.shape[0] - right_border) / 2).astype(int)
+    back_border = get_upper_bound(s2, .5e-6)
+    front_border = get_lower_bound(s2, .5e-6)
 
-    m0 = s0 < .00015
-    top_border = np.argmax(m0)
-    if top_border == 0:
-        top_border = s0.shape[0]
-    top_border
+    resized_image = image[:top_border, avg_lr_border:s1.shape[0] - avg_lr_border, front_border:back_border]
+    if verbose > 1:
+        print('Image resized from {} to {}'.format(image.shape, resized_image.shape))
+    return resized_image
 
-    middle = np.floor(s1.shape[0] / 2)
-    idx = np.arange(s1.shape[0])
-    m1 = s1 < .0002
-    right_border = np.argmax(np.where(idx > middle, m1, False))
-    left_border = np.argmax(~np.where(idx < middle, m1, False))
-    left_border, right_border
 
-    middle = np.floor(s2.shape[0] / 2)
-    idx = np.arange(s2.shape[0])
-    m2 = s2 < .01
-    back_border = np.argmax(np.where(idx > middle, m2, False))
-    front_border = np.argmax(~np.where(idx < middle, m2, False))
-    front_border, back_border
+def preprocess_tsa_data():
+    """Preprocess all a3d files for training and persist to disk."""
+    # get list of scans
+    scans = pd.read_csv(path_labels)
+    scans['subject_id'] = scans.Id.str.split('_').str[0]
+    scans['zone_num'] = scans.Id.str.split('Zone').str[1].astype(int)
 
-    return image[:top_border, left_border:right_border, front_border:back_border]
+    # preprocess each scan
+    blacklist = {}
+    for subject_id in tqdm(scans.subject_id.unique()):
+        image = tsa.read_data(path.join(path_a3d, subject_id + '.a3d'))
+        image = image.transpose(2, 0, 1)  # axis are now height (top) x width (side) x  depth (front)
+        cropped_image = crop_image(image)
+        if cropped_image.shape[0] < 550 or cropped_image.shape[1] < 300 or cropped_image.shape[1] > 520 or cropped_image.shape[2] < 100 or cropped_image.shape[2] > 300:
+            blacklist[subject_id] = cropped_image.shape
+            with open('blacklist.json', 'w') as f:
+                json.dump(blacklist, f, indent=4)
+        preprocessed_image = preprocess_image(cropped_image)
+        resized_image = resize(preprocessed_image, (IMAGE_DIM, IMAGE_DIM, IMAGE_DIM), mode='constant')
+        np.save(path.join(path_cache, subject_id + '.npy'), resized_image)
+
+        # save a cross section for validating cropping
+        save_image(
+            path.join(path_plots, subject_id + '.png'),
+            tsa.convert_to_grayscale(resized_image)[:, :, np.floor(resized_image.shape[2] / 2).astype(int) - 5],
+        )
 
 
 if __name__ == '__main__':
