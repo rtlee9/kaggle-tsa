9edbe3a3214349d4e0b5d58636ca90d1537bbc31
Ryan Lee <ryantlee9@gmail.com>
2017-11-30 17:54:02 -0800

    Full dataset

--- src/pipeline.py
+++ src/pipeline.py
@@ -183,12 +183,12 @@ def get_data_loaders(threat_zone):
     test_transformations = [ZoneCrop(threat_zone), Resize(), Filter(), ToTensor()]  # base transformations
     dataset_train = TsaScansDataset(
         threat_zone=threat_zone,
-        keep_label_idx=['9ec808303497389de113d609a65c7935'],
+        keep_label_idx=label_idx_train,
         blacklist=blacklist,
         transforms=transforms.Compose(train_transformations)
     )
     assert type(dataset_train.__getitem__(0)['image']) == torch.FloatTensor
-    # assert (len(dataset_train) == 2206)
+    assert (len(dataset_train) == 2206)
     assert (dataset_train.__getitem__(0))
     assert (dataset_train.__getitem__(len(dataset_train) - 1))
     targets = dataset_train.labels.Probability
