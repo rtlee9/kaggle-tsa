651256fe3474a257b47b485724ed917ff90b0af4
Ryan Lee <ryantlee9@gmail.com>
2017-11-30 17:49:38 -0800

    Add transfoer learning: VGG weights

--- src/model.py
+++ src/model.py
@@ -1,29 +1,29 @@
 """Convolutional neural net (TSA net)."""
-
 import torch.nn as nn
+from torchvision import models
 
 
 class TsaNet(nn.Module):
     """3D convolutional neural net to predict threat probability by threat zone."""
 
-    def __init__(self, num_classes=1):
+    def __init__(self, num_classes=1, transfer=None):
         """Initialize TsaNet structure."""
         super(TsaNet, self).__init__()
         self.features = nn.Sequential(  # initial torch.Size([1, 1, 32, 32, 32])
 
-            nn.Conv3d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.Conv3d(1, 64, kernel_size=3, stride=1, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool3d(kernel_size=2, stride=2),
 
-            nn.Conv3d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool3d(kernel_size=2, stride=2),
 
         )
 
         self.classifier = nn.Sequential(
-            # nn.Dropout(),
-            nn.Linear(32 * 8 ** 3, num_classes),
+            nn.Dropout(),
+            nn.Linear(64 * 8 ** 3, num_classes),
             nn.Sigmoid(),
         )
 
@@ -37,9 +37,19 @@ class TsaNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
+        # transfer weights
+        if transfer:
+            vgg = models.vgg16(pretrained=True)
+            w1_vgg = vgg.features[0].weight
+            w2_vgg = vgg.features[2].weight
+            d = self.state_dict()
+            d['features.0.weight'] = w1_vgg.unsqueeze(1).data
+            d['features.4.weight'] = w2_vgg.unsqueeze(1).data
+            self.load_state_dict(d)
+
     def forward(self, x):
         """Net forward pass."""
         x = self.features(x)
-        x = x.view(x.size(0), 32 * 8 ** 3)
+        x = x.view(x.size(0), 64 * 8 ** 3)
         x = self.classifier(x)
         return x.squeeze()
