ea2f1058db2dd9fe5636a6b27f03bbc6cb866683
Ryan Lee <ryan.lee@wellsheet.com>
2017-12-04 20:39:10 -0800

    Adjust crop zones

--- src/zones.py
+++ src/zones.py
@@ -22,16 +22,16 @@ right_left_map = {v: k for k, v in left_right_map.items()}
 
 image_dims = (IMAGE_DIM, IMAGE_DIM, IMAGE_DIM)
 common_threat_body_map = {
-    16: crop_dims(image_dims, top=25, left=65, right=105),
-    14: crop_dims(image_dims, top=30, bottom=15, left=65, right=105),
-    12: crop_dims(image_dims, top=40, bottom=25, left=60, right=100),
-    10: crop_dims(image_dims, top=70, bottom=33, left=60, right=105),
-    7: crop_dims(image_dims, top=85, bottom=57, left=55, right=105),
-    3: crop_dims(image_dims, top=115, bottom=80, left=76, right=126),
-    4: crop_dims(image_dims, bottom=83, left=70, right=126),
-    9: crop_dims(image_dims, top=70, bottom=33, left=49, right=74),
-    5: crop_dims(image_dims, top=100, bottom=65, left=30, right=98, back=94),
-    17: crop_dims(image_dims, top=100, bottom=65, left=30, right=98, front=75),
+    16: crop_dims(image_dims, top=20, left=70, right=125),
+    14: crop_dims(image_dims, top=32, bottom=13, left=73, right=122),
+    12: crop_dims(image_dims, top=47, bottom=25, left=61, right=120),
+    10: crop_dims(image_dims, top=75, bottom=30, left=67, right=120),
+    7: crop_dims(image_dims, top=90, bottom=65, left=57, right=118),
+    3: crop_dims(image_dims, top=117, bottom=83, left=76, right=126),
+    4: crop_dims(image_dims, bottom=92, left=70, right=126),
+    9: crop_dims(image_dims, top=70, bottom=30, left=45, right=83),
+    5: crop_dims(image_dims, top=100, bottom=75, left=30, right=98, back=94),
+    17: crop_dims(image_dims, top=100, bottom=75, left=30, right=98, front=75),
 }
 
 
